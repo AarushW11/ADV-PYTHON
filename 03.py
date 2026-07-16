f18b99d49c705adc064bf7d1b7f38a433a146367
AarushW11 <aarushwagh0@gmail.com>
2026-07-16 10:29:39 +0530

    Add files via upload

---
+++ 03.py
@@ -0,0 +1,59 @@
+class book:
+    def __init__(self , name):
+        self.name = name
+        self.available = True
+
+class lilbrary:
+    def __init__(self):
+        self.books = {}
+
+    def add_book(self):
+        name = print("Enter the name of the book:")
+        self.books[name]=book[name]
+
+    def borrow_book(self):
+        name = input("enter book's name to borrow:")
+
+        if name in self.books:
+            if self.books[name].available:
+                self.books[name].available = False
+     
+                print("book borrowed")
+                
+            else:
+            
+                print("Book is already borrowed")
+                
+        else:
+            print("Book not found") 
+
+            def display_books(self):
+                print("books in library")
+                for book in self.books.values():
+                    if book.available:
+                        print(book.name,"available")
+
+                    else:
+                        print(book.name,"borrowed")
+
+Library = Library()
+
+while True:
+    print("library management system")
+    print("1.add book")
+    print("2.borrow book")
+    print("3.return book")
+    print("4.display books")
+    print("5.exit")
+
+    choice = int(input("enter your choice"))
+
+    if choice == 1:
+        library.add_book()
+    elif choice == 2:
+        library.borrow_book()
+    
+
+
+        
+    
